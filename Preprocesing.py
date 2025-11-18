4b40e07d033834ace78ff4aee3468a9fa81600a3
Youssef Mustafa <115989856+youssef969@users.noreply.github.com>
2025-11-18 22:35:03 +0200

    Final Stage of Text Cleaning and Preprocessing

--- Preprocesing.py
+++ Preprocesing.py
@@ -3,6 +3,16 @@ import pandas as pd
 
 class Process_column:
 
+    @staticmethod
+    def NumberofUniqueWords(dataset:pd.DataFrame,Column:str):
+        unique_words = set()
+        for text in dataset[Column].dropna(): 
+            for word in text.split():
+                unique_words.add(word)
+        num_unique_words = len(unique_words)
+        print("Number of unique words:", num_unique_words)
+
+
     def __init__(self,Column_name:str,Dataset:pd.DataFrame):
         self.Column_name = Column_name
         self.Dataset = Dataset
