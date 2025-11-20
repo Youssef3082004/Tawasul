88352f224916ac8b71c2017e10f647b15fd791de
Youssef Mustafa <awadallayossef@gmail.com>
2025-11-20 20:26:53 +0200

    deploy LSTM model in huggingface

---
+++ LSTM/app.py
@@ -0,0 +1,80 @@
+from fastapi import FastAPI
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import nltk
+import re
+import pickle
+
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+    nltk.download('punkt_tab') 
+
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
+
+try:
+    nltk.data.find('corpora/wordnet')
+except LookupError:
+    nltk.download('wordnet')
+_stopwords = stopwords.words("english")
+
+
+app = FastAPI()
+
+
+model = load_model("sentiment_model.keras")
+
+with open("tokenizer.pickle", "rb") as f:
+    tokenizer = pickle.load(f)
+
+with open("label_encoder.pkl", "rb") as f:
+    label_encoder = pickle.load(f)
+
+
+def _remove_Stopwords(text: list):
+    return [x for x in text if x not in _stopwords]
+
+def _Lemmatizong_data(text: list):
+    Lemmarizer = WordNetLemmatizer()
+    try:
+        return [Lemmarizer.lemmatize(word=word, pos="v") for word in text]
+    except Exception:
+        return text
+
+def CleanText(Text: str):
+    Text = Text.lower() 
+    Text = re.sub(pattern=r"[^\w\s]", repl=" ", string=Text)
+    Text = re.sub(pattern=r"\b[a-z]+n\s+t\b", repl=" ", string=Text)
+    Text = re.sub(pattern=r"\b[a-z]{1}\b", repl=" ", string=Text)    
+    Text = word_tokenize(Text) 
+    Text = _remove_Stopwords(Text)
+    Text = _Lemmatizong_data(Text)
+    return " ".join(Text)
+
+
+@app.post("/Classify")
+def ClassifyText(NoteText: str):
+    try:
+        cleaned_text = CleanText(Text=NoteText)
+        
+        text_list = [cleaned_text]
+        seq = tokenizer.texts_to_sequences(text_list)
+        padded = pad_sequences(seq, maxlen=25, padding="post")
+
+        y_pred = model.predict(padded)
+        label = y_pred.argmax(axis=1)
+
+        label_name = label_encoder.inverse_transform(label)[0]
+
+        return {"Emotion": label_name, "Percentage": float(y_pred[0][label[0]])}
+    
+    except Exception as e:
+        return {"Error": str(e)}
